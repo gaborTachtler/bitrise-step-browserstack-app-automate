3bfe0b4a6d6bfcbb34261839c792a7f4c67a91a7
Lizi Boros <37154593+boroslizi@users.noreply.github.com>
2022-02-01 09:41:49 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -56,4 +56,4 @@ echo "---Save report---"
 curl -u "$browserstack_username":"$browserstack_access_key" -X GET "https://api-cloud.browserstack.com/app-automate/espresso/v2/builds/$build_id/sessions/$session_id/report" > $BITRISE_DEPLOY_DIR/report.xml
 
 echo "---Save video---"
-curl -u "$browserstack_username":"$browserstack_access_key" -X GET "https://www.browserstack.com/s3-upload/bs-video-logs-usw/s3-us-west-1/$session_id/video-$session_id.mp4" > $BITRISE_DEPLOY_DIR/video.mp4
+curl -o $BITRISE_DEPLOY_DIR/video.mp4 "https://www.browserstack.com/s3-upload/bs-video-logs-use/s3/$session_id/video-$session_id.mp4"
